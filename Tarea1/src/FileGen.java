d9ec25c8873ca44a12efc48d5bc954120eb0aa36
Max <mvargas33@protonmail.com>
2019-09-25 00:14:39 -0300

    Ahora si crea archivos de exactamente 1Kb. Usado FileOutputStream

--- Tarea1/src/FileGen.java
+++ Tarea1/src/FileGen.java
@@ -1,9 +1,12 @@
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.util.Random;
 
 public class FileGen{
     public static void genBlocks(int numOfBlocks, int BLOCK_SIZE, String filePrefix) throws Exception{
-        FileWriter outputStream = null;
+        //FileWriter outputStream = null;
+        //PrintWriter p = null;
+        FileOutputStream f = null;
         String baseDir = System.getProperty("user.dir") + "\\";
         Random rd = new Random();
         byte[] arr = new byte[BLOCK_SIZE];
@@ -12,15 +15,24 @@ public class FileGen{
             try {
                 file += (numOfBlocks - 1) + ".wtf";
                 rd.nextBytes(arr); // Random de bytes
-                outputStream = new FileWriter(baseDir + file);
+                //outputStream = new FileWriter(baseDir + file);
+                //p = new PrintWriter(baseDir + file);
+                f = new FileOutputStream(baseDir + file);
                 int c = BLOCK_SIZE - 1;
-                while (c != -1 ) {
-                    outputStream.write(arr[c]);
+                f.write(arr);
+                /*while (c != -1 ) {
+                    //outputStream.write(arr[c]);
+                    p.print((char) arr[c]);
                     c--;
                 }
+                */
             } finally {
-                if (outputStream != null) {
+                /*if (outputStream != null) {
                     outputStream.close();
+                }*/
+                if ( f != null )
+                {
+                    f.close();
                 }
             }
             numOfBlocks--;
@@ -28,7 +40,7 @@ public class FileGen{
         }
     }
 
-    public static void main(String... args){
+    public static void main(String... args) throws Exception{
         genBlocks(2, 1024, "X_");
         genBlocks(3, 1024, "Y_");
     }
