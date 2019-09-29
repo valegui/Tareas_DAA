9acdd8f630af7365fe9abd1649baaee19f05fee7
Valeria Guidotti <valeriaguidotti@gmail.com>
2019-09-29 19:11:48 -0300

    add experiment methods to Experiment

--- Tarea1/src/Experiments.java
+++ Tarea1/src/Experiments.java
@@ -1,3 +1,5 @@
+import java.io.File;
+
 public class Experiments {
 
     public static void generateFile(int N, String directory){
@@ -9,18 +11,33 @@ public class Experiments {
         }
     }
 
-    public static void cleanDirectory(String directory){
 
+    public static void deleteListFilesOnFolder(final File folder) {
+        for (final File fileEntry : folder.listFiles()) {
+            if (fileEntry.isDirectory()) {
+                deleteListFilesOnFolder(fileEntry);
+            }
+            fileEntry.delete();
+        }
     }
 
     public static void experimentAdaptedRAM(int N, int m, String directory){
         generateFile(N, directory);
-        cleanDirectory(directory);
+        AdaptedRAM experiment = new AdaptedRAM(m, N);
+        experiment.setDirRow(directory);
+        experiment.setDirX(directory);
+        experiment.setDirY(directory);
+        int distance = experiment.calculateDistance();
+        int IO = experiment.getIO();
+        deleteListFilesOnFolder(new File(directory));
 
     }
 
     public static void experimentGridRAM(int N, int m, String directory){
         generateFile(N, directory);
-        cleanDirectory(directory);
+        RAMConFronteras exp = new RAMConFronteras(directory, directory, directory, m, N);
+        int distance = exp.calcAllDist();
+        int IO = exp.getIO();
+        deleteListFilesOnFolder(new File(directory));
     }
 }
