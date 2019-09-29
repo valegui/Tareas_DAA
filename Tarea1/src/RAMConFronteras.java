4df4620f084a2483b786d9a1f96c763e8f7769b4
Max <mvargas33@protonmail.com>
2019-09-29 00:06:29 -0300

    Diagonal psudo implementada, falta sacar lo estatico

--- Tarea1/src/RAMConFronteras.java
+++ Tarea1/src/RAMConFronteras.java
@@ -65,6 +65,8 @@ public class RAMConFronteras {
         int cantidadCaracteres_X = 0, cantidadCaracteres_Y = 0; // Contador para generar fronteras superior e izquierdas en primera columna y primera fila
         String str_X, str_Y; // Substrings actuales
         int[] izq, sup; // Fronteras actuales
+        int[] diagonales = new int[total_Y];
+        diagonales[0] = 0;
 
         // Itero sobre filas de X
         for(int a = 0; a < total_X; a++){
@@ -99,7 +101,7 @@ public class RAMConFronteras {
                 }
 
                 // En este punto ya tengo ambas fronteras y strings definidas, calculo nuevas fronteras y sobreescribo
-                frontiersInFiles[j] = calcDist(str_X, str_Y, izq, sup); // Guardo en posición que no voy a usar mas
+                frontiersInFiles[j] = calcDist(str_X, str_Y, izq, sup, diagonales[j]); // Guardo en posición que no voy a usar mas
                 // TODO: Aquí guardar ambas fronteras en el archivo j-ésimo
             }
         }
@@ -113,26 +115,30 @@ public class RAMConFronteras {
      * Algoritmo principal: Itera fila por fila y retorna fronteras
      * @return En 0: Frontera inferior, 1: Frontera derecha
      */
-    public static int[][] calcDist(String x, String y, int[] columnaIzquierda, int[] filaSuperior_Y) throws Exception{
-        int largo_x = x.length();
-        int largo_y = y.length();
+    public static int[][] calcDist(String x, String y, int[] columnaIzquierda, int[] filaSuperior_Y, int laMalditaDiagonal) throws Exception{
+        int largo_x = x.length();int largo_y = y.length(); // Para no recalcular
+        int l, p; // Para no recalcular
 
-        int l, p;
         // Verificación de largos de filas proporcionadas y largos de Strings
         if(largo_x != (l = columnaIzquierda.length)){
             System.out.println("Frontera columna izquierda anterior administrada no coincide con el largo de string X: " + largo_x + " (largo real) vs " + l);
             throw new Exception();
         }
-        if(largo_y + 1 != (p = filaSuperior_Y.length)){ // +1 porque contiene el valor de la diagonal
+        if(largo_y != (p = filaSuperior_Y.length)){ // +1 porque contiene el valor de la diagonal
             System.out.println("Frontera superior anterior administrada no coincide con el largo de string Y: " + largo_y + " (largo real) vs " + p);
             throw new Exception();
         }
 
         /***************************/
-        // Computa para cada caracter de X
-        for (int i = 0; i < largo_x; i++){
-            computarFila(i, x, y, largo_y, filaSuperior_Y, columnaIzquierda);
+        // Primer caracter de X diferente
+        int diag = computarFilaB(0, x.charAt(0), y, largo_y, filaSuperior_Y, columnaIzquierda, laMalditaDiagonal);
+
+        // Resto de caracteres de X
+        for (int i = 1; i < largo_x; i++){
+            diag = computarFilaB(i, x.charAt(i), y, largo_y, filaSuperior_Y, columnaIzquierda, diag);
         }
+
+        // TODO: En diag queda el valor a guardar en el arreglo global de diagonales
         // En ant queda la ultima fila y retorna esta como frontera superior, y
         //System.out.println(Arrays.toString(filaSuperior_Y));
         //System.out.println(Arrays.toString(columnaIzquierda));
@@ -141,29 +147,39 @@ public class RAMConFronteras {
     }
 
     /**
+     * Debe retornar el resultado de la diagonal de la última fila.
      * Computa una fila basado en los valores de 'ant' y columna lateral.
      * También actualiza el valore columna lateral de la mismafila por sú último valor recién calculado, para retornar al final
-     * @param fila : fila actual
+     * @param fila índice de fila actual
+     * @param x caracter en x
+     * @param y String y
+     * @param largo_y largo de y para no recalcular
+     * @param ant valores de fila anterior
+     * @param izq valores de columna izquierda anterior
+     * @param diagonal diagonal de posicion 0, necesario para el primer cálculo
+     * @return
      */
-    public static void computarFila(int fila, String x, String y, int largo_y, int[] ant, int[] columna_lateral) {
-        char s = x.charAt(fila); // caracter de x
+    public static int computarFilaB(int fila, char x, String y, int largo_y, int[] ant, int[] izq, int diagonal) {
+        int [] nueva = new int[largo_y]; // nueva Fila
         char z; // caracter de y actual
+        int NW_val, W_val, N_val;int NW = 1; // Valores | North West (diagonal) | 1 hasta que se diga lo contrario
 
-        // VALORES
-        int NW = 1; // North West (diagonal) | 1 hasta que se diga lo contrario
-        int NW_val;
-        int W_val;
-        int N_val;
+        // Primera iteracion
+        z = y.charAt(0); // Saco y
+        if(x == z)
+            NW = 0;
+        NW_val = diagonal + NW; // Ocupo valor directo de la diagonal
+        N_val = ant[0];
+        W_val = izq[fila];
 
-        int [] nueva = new int[largo_y + 1]; // nueva Fila
-        nueva[0] = columna_lateral[fila]; // La frontera lateral tiene tantos valores como caracteres de X; no repite el valor superior que ya contiene la primera fila
+        nueva[0] = Math.min(NW_val, Math.min(N_val, W_val)); // Primer valor de la fila actual
 
-        // Recorremos la nueva fila
-        for(int j = 1; j <= largo_y; j++){
-            z = y.charAt(j-1); // caracter de Y
+        // Recorremos el resto de la fila
+        for(int j = 1; j < largo_y; j++){
+            z = y.charAt(j); // caracter de Y
 
             NW = 1; // Reset de North West
-            if(s == z) // Si son el mismo caracter, diagonal vale 0, sino 1
+            if(x == z) // Si son el mismo caracter, diagonal vale 0, sino 1
                 NW = 0;
             NW_val = ant[j-1] + NW;
             N_val = ant[j] + 1;
@@ -173,12 +189,14 @@ public class RAMConFronteras {
         }
         //System.out.println(Arrays.toString(nueva)); // ultima fila
         //System.out.println(nueva[largo_y]);
-        // Guardamos el ultimo valor en el array que ya no vamos a usar, generamos la frontera derecha, (izquierda) para siguiente iteracion
-        columna_lateral[fila] = nueva[largo_y];
+
+        W_val = izq[fila]; // Guardamos valor de la diagonal para retornar en un entero que ya no vamos a usar
+        izq[fila] = nueva[largo_y - 1]; // Guardamos el ultimo valor en el array que ya no vamos a usar, generamos la frontera derecha, (izquierda) para siguiente iteracion
 
         // Nueva fila pasa a ser la anterior
         System.arraycopy(nueva, 0, ant, 0, largo_y + 1); // +1 porque guarda el diagonal
 
+        return W_val;
     }
 
     public void writeFrontierColumnToFile(int subMatrixID){
